675f46a50e5fa882864a53c30380b84e3f82e30b
Kosschaser <Dmytromailto@gmail.com>
2022-08-11 23:47:32 +0300

    main functionality finished

--- src/test/java/Mail.java
+++ src/test/java/Mail.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
@@ -19,6 +21,9 @@ public class Mail {
     WebDriver driver;
     private static final String LOGIN = "testotestovich7@gmail.com";
     private static final String PASS = "testpassword1029";
+    private static final String MESSAGE = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    private static final String MAIL_TO = "Dmytromailto@gmail.com";
+
 
     public void waitForVisibility(long timeToWait, WebElement element) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeToWait));
@@ -66,6 +71,23 @@ public class Mail {
         WebElement composeMail = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='z0']")));
         composeMail.click();
+
+        WebElement recipient = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable((By.xpath("//textarea[@name='to']"))));
+        WebElement messageField = driver.findElement(By.id(":9n"));
+        WebElement sendMail = driver.findElement(By.id(":87"));
+
+        recipient.sendKeys(MAIL_TO);
+        messageField.sendKeys(MESSAGE);
+        sendMail.click();
+
+        WebElement sentList = driver.findElement(By.id(":4a"));
+        sentList.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.urlContains("#sent"));
+        List<WebElement> messagesSent = driver.findElements((By.xpath("//tr[@class='zA yO']")));
+
+        Assert.assertTrue(!messagesSent.isEmpty());
 
     }
 
